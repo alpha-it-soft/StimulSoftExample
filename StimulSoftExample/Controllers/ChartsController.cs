44f4ab2cee8273bb34fbba2808cce365c5e6821a
Daniel Franciuc <daniel.franciuc@alpha-it.ro>
2024-02-19 09:41:02 +0100

    Projektdateien hinzufügen.

---
+++ StimulSoftExample/Controllers/ChartsController.cs
@@ -0,0 +1,80 @@
+﻿using Stimulsoft.Report.Dictionary;
+using Stimulsoft.Report.Mvc;
+using Stimulsoft.Report;
+
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using static Stimulsoft.Base.Drawing.StiFontReader;
+using System.Text;
+
+namespace StimulSoftExample.Controllers
+{
+    public class ChartsController : Controller
+    {
+       
+        static ChartsController()
+        {
+            // How to Activate
+            Stimulsoft.Base.StiLicense.Key = ConfigurationManager.AppSettings["StiLicenseKey"];
+            //Stimulsoft.Base.StiLicense.LoadFromFile("license.key");
+            //Stimulsoft.Base.StiLicense.LoadFromStream(stream);
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetReport()
+        {
+          
+     
+
+                    // Create the report object
+                    var report = new StiReport();
+                  
+                    report.Load(Server.MapPath("~/Reports/Barchart.mrt"));
+ 
+                    //set report name
+                    report.ReportName = "Balkendiagramm_" + DateTime.Now.ToString("yyyy_MM_dd HH_mm");
+ 
+                    
+           
+
+            // Remove all connections from the report template
+            report.Dictionary.Databases.Clear();
+
+            // Register DataSet object
+            int busobjLevel = 1;
+
+            StimulSoftExample.Models.StiBusinessObject stiObj = new StimulSoftExample.Models.StiBusinessObject();
+            string path = Server.MapPath("~/Data/Data.txt");
+            string text=System.IO.File.ReadAllText(path, Encoding.UTF8);
+            try
+            {
+                stiObj=Newtonsoft.Json.JsonConvert.DeserializeObject<StimulSoftExample.Models.StiBusinessObject>(text);
+            }
+            catch (Exception x)
+            {
+                stiObj = new StimulSoftExample.Models.StiBusinessObject();
+            }    
+            report.RegBusinessObject("Data", stiObj);
+            //string json=Newtonsoft.Json.JsonConvert.SerializeObject(stiObj);
+            //string path = "C:\\temp\\Data.txt";
+            //System.IO.File.WriteAllText(path, json, Encoding.UTF8);
+            report.Dictionary.SynchronizeBusinessObjects(busobjLevel);
+
+            return StiMvcViewer.GetReportResult(report);
+
+        }
+
+        public ActionResult ViewerEvent()
+        {
+            return StiMvcViewer.ViewerEventResult();
+        }
+    }
+}
---
+++ StimulSoftExample/Models/StiBusinessObject.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace StimulSoftExample.Models
+{    
+    public class StiBusinessObject
+    {
+        public StiBusinessObject()
+        {
+            Info = new StiInfo();
+            Summary = new List<CsvSummary>();
+            ChartPages = new List<StiPage>();
+            Table1 = new List<StiTable>();
+            Table2 = new List<StiTable>();
+            Table3 = new List<StiTable>();
+            Table4 = new List<StiTable>();
+        }
+        public StiInfo Info { get; set; }
+        public List<CsvSummary> Summary { get; set; }
+        public List<StiPage> ChartPages { get; set; }
+        public List<StiTable> Table1 { get; set; }
+        public List<StiTable> Table2 { get; set; }
+        public List<StiTable> Table3 { get; set; }
+        public List<StiTable> Table4 { get; set; }
+    }
+    public class StiInfo
+    {
+        public string InspectionTask { get; set; }
+        public string PartNumber { get; set; }
+        public string PartName { get; set; }
+        public DateTime StartReport { get; set; }
+        public string ChangeStatus { get; set; }
+        public int ControlLimit { get; set; }
+        public string UserLabel { get; set; }
+        public string ReportTypeISRA { get; set; }
+        public int CountCsvReference { get; set; }
+        public int CountCsv { get; set; }
+        public int CountCharts { get; set; }
+        public int CountQlv { get; set; }
+        public string Template { get; set; }
+
+        public StiInfo()
+        {
+
+        }
+        
+    }
+    public class StiPage
+    {
+        public int PageNumber { get; set; }
+        public bool Visible { get; set; }
+        public string Base64 { get; set; }
+
+        public StiPage()
+        {
+            /*this.PageNumber = PageNumber;
+            this.Base64 = Base64;
+            this.Visible = (this.Base64 != "");
+            */
+        }
+    }
+
+    public class StiTable
+    {
+        public string PointName { get; set; }
+        public string Characteristic { get; set; }
+        public string DisplayCharacteristic { get; set; }
+
+        public string Selectedxis { get; set; }
+
+        public double OT { get; set; }
+        public double UT { get; set; }
+        public double SW { get; set; }
+
+        public string DisplayOT { get { return OT.ToString("0.00"); } }
+        public string DisplayUT { get { return UT.ToString("0.00"); } }
+        public string DisplaySW { get { return SW.ToString("0.00"); } }
+
+        public double PointMin { get; set; }
+        public double PointMax { get; set; }
+        public double PointMed { get; set; }
+        public double PointRange { get; set; }
+
+        public double PointX { get; set; }
+        public double PointY { get; set; }
+        public double PointZ { get; set; }
+        public string PointType { get; set; }
+
+        public double? PointQlvL { get; set; }
+        public double? PointQlvU { get; set; }
+
+        public double DisplayMax { get; set; }
+        public double DisplayMin { get; set; }
+
+        public string BlueLimit { get; set; }
+        public string BlueLimitComment { get; set; }
+        public string QlvComment { get; set; }
+
+        public bool LastPartIsNok { get; set; }
+        public bool isBM { get; set; }
+
+        public string ListValues_IO { get; set; }
+        public string ListValues_NIO { get; set; }
+        public string ListValues_Tags { get; set; }
+        public string ListValues_NIO_Tags { get; set; }
+        public string ListValues_NA { get; set; }
+        public string ListComment_IO { get; set; }
+        public string ListComment_NIO { get; set; }
+        public string ListComment_NA { get; set; }
+
+        public string LowerLimit { get; set; }
+        public string UpperLimit { get; set; }
+
+        public string LowerLimitComment { get; set; }
+        public string UpperLimitComment { get; set; }
+
+        public string ListArguments { get; set; }
+        public string IdentRow { get; set; }
+
+        public double? LastCsvDifference { get; set; }
+        public double? LastCsvValue { get; set; }
+        public double? TotalValue { get; set; }
+        public double? TotalDifference { get; set; }
+
+        public string DisplayPointAxisValue
+        {
+            get
+            {
+                return "  X " + PointX.ToString("0.00") + " Y " +
+                    PointY.ToString("0.00") + " Z " +
+                    PointZ.ToString("0.00") + "   /   " +
+                    PointType;
+            }
+        }
+        public string DisplayQlvValue
+        {
+            get
+            {
+                if (PointQlvL.HasValue & PointQlvU.HasValue)
+                    return "Aktuelle QLV [" + PointQlvL.Value.ToString("0.00") + " -> " + PointQlvU.Value.ToString("0.00") + "]";
+                else
+                    return "";
+            }
+        }
+
+        public string DisplaySummaryValue
+        {
+            get
+            {
+                List<string> ListParameter = new List<string>();
+                ListParameter.Add("Min. " + PointMin.ToString("0.00"));
+                ListParameter.Add("ø " + PointMed.ToString("0.00"));
+                ListParameter.Add("Max. " + PointMax.ToString("0.00"));
+                ListParameter.Add("R " + PointRange.ToString("0.00"));
+
+                return string.Join("  ", ListParameter.ToArray());
+            }
+        }
+
+        public string DisplaySummaryTotal
+        {
+            get
+            {
+                List<string> ListParameter = new List<string>();
+                if (TotalValue.HasValue)
+                    ListParameter.Add("Ist ø: " + TotalValue.Value.ToString("0.00"));
+                else
+                    ListParameter.Add("Ist ø: " + 0.ToString("0.00"));
+
+                if (TotalDifference.HasValue)
+                    ListParameter.Add("Abw: " + TotalDifference.Value.ToString("0.00"));
+                else
+                    ListParameter.Add("Ist: " + 0.ToString("0.00"));
+
+                if (ListParameter.Count > 0)
+                    return string.Join("\n", ListParameter.ToArray());
+
+                return string.Empty;
+            }
+        }
+
+
+        public string DisplaySummaryLastCsv
+        {
+            get
+            {
+                List<string> ListParameter = new List<string>();
+                if (LastCsvValue.HasValue)
+                    ListParameter.Add("Ist: " + LastCsvValue.Value.ToString("0.00"));
+                else
+                    ListParameter.Add("Ist: " + 0.ToString("0.00"));
+
+                if (LastCsvDifference.HasValue)
+                    ListParameter.Add("Abw: " + LastCsvDifference.Value.ToString("0.00"));
+                else
+                    ListParameter.Add("Ist: " + 0.ToString("0.00"));
+
+                if (ListParameter.Count > 0)
+                    return string.Join("\n", ListParameter.ToArray());
+
+                return string.Empty;
+            }
+        }
+    }
+
+    public class CsvSummary
+    {
+        public string ComponentID { get; set; }
+        public string Type { get; set; }
+        public string ProductionLine { get; set; }
+        public string InspectionCategories { get; set; }
+        public DateTime InspectionDate { get; set; }
+        public string Description { get; set; }
+        public string Messpoints { get; set; }
+        public double PistResult { get; set; }
+        public int Pos { get; set; }
+
+        public override string ToString()
+        {
+            return this.Pos +
+                " - " + this.ComponentID +
+                 " - " + this.Type +
+                  " - " + this.ProductionLine +
+                   " - " + this.InspectionCategories +
+                    " - " + this.InspectionDate +
+                     " - " + this.Description +
+                      " - " + this.Messpoints +
+                       " - " + this.PistResult + "%";
+        }
+    }
+}
